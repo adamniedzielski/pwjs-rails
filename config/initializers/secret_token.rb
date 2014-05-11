d1fda556b562e14889eb5446a65e2ac0f282262f
Adam Niedzielski <adamsunday@gmail.com>
2014-05-11 10:41:46 +0200

    Bootstrap Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PwjsRails::Application.config.secret_key_base = '5ad2452795dc91d5161d6f96360de15280542d602731810a198d892f7e5dfbc0a0891e598e5a68f19e4e24a6e4f083c336c92f2e9cd2266dbb9bb6586835458a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PwjsRails::Application.config.session_store :cookie_store, key: '_pwjs-rails_session'
